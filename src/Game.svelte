eb9e63fa6e8e8cca18fc0c7344cff8ed4ce6672d
mib <mib@mib.dev>
2021-09-30 11:50:22 +0200

    Make grid size dynamic

--- src/Game.svelte
+++ src/Game.svelte
@@ -2,29 +2,24 @@
 	import { onMount } from 'svelte';
 	import Cell from './Cell.svelte';
 	
-	let gridSize = 3;
-
+	export let gridSize = 3;
 	$: cells = [];
-
-	function addCell(el) {
-		cells = [...cells, el];
-	}
 
 	onMount(() => {
 		for (let x = 0; x < gridSize; x++) {
 			for (let y = 0; y < gridSize; y++) {
 				var cell = document.createElement('div');
-				cell.className += "cell";
-				cell.innerHTML = `${x}, ${y}`;
-				addCell(cell);
+				cells = [...cells, cell];
 			}
 		}
+
+		document.getElementById('grid').style['grid-template-columns'] = `repeat(${gridSize}, minmax(0, 1fr))`;
 	});
 </script>
 
 <div id="grid">
 	{#each cells as _, i}
-		<Cell assignment="{i}" />
+		<Cell x={(i % gridSize) + 1} y={Math.floor(i/gridSize)+1} assignment={i} />
 	{/each}
 </div>
 
@@ -33,6 +28,5 @@
 		height: 100%;
 		width: 100%;
 		display: grid;
-		grid-template-columns: repeat(3, minmax(0, 1fr));
 	}
 </style>
